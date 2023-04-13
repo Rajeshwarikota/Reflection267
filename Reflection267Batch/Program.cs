89933f5cc104022f847a8e97464ed8f43f982565
Rajeshwarikota <kotarajeswari003.com>
2023-04-13 23:09:02 +0530

    [Rajeshwari kota] Adding Finding Nearest number

--- Reflection267Batch/Program.cs
+++ Reflection267Batch/Program.cs
@@ -11,6 +11,13 @@ namespace Reflection267Batch
         static void Main(string[] args)
         {
             Console.WriteLine("....REFLECTION....");
+            int n = 17;
+            int closest = Reflection.GetClosestEvenNumber(n);
+            Console.WriteLine($"The closest number to {n} with all even digits is {closest}");
+
+            int m = -33;
+            int closestM = Reflection.GetClosestEvenNumber(m);
+            Console.WriteLine($"The closest number to {m} with all even digits is {closestM}");
             Console.ReadLine(); 
         }
     }
---
+++ Reflection267Batch/Reflection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection267Batch
+{
+    class Reflection
+    {
+        public static int GetClosestEvenNumber(int n)
+        {
+            int absN = Math.Abs(n);
+            int closest = int.MaxValue;
+            // Iterate over even numbers less than or equal to absN
+            for (int i = 0; i <= absN; i += 2)
+            {
+                int temp = i;
+                bool allEven = true;
+                // Iterate over digits of i and check if they are all even
+                while (temp > 0)
+                {
+                    int digit = temp % 10;
+                    if (digit % 2 != 0)
+                    {
+                        allEven = false;
+                        break;
+                    }
+                    temp /= 10;
+                }
+                // Update closest if i has all even digits and is closer to absN
+                if (allEven && Math.Abs(absN - i) < Math.Abs(absN - closest))
+                {
+                    closest = i;
+                }
+            }
+            // Negate closest if n is negative
+            if (n < 0)
+            {
+                closest = -closest;
+            }
+            return closest;
+        }
+    }
+}
